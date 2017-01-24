d5c51ed8398beead14861e51a0f2a92b6c5a5818
Martin Albisetti <argentina@gmail.com>
2017-01-24 10:54:52 +0100

    Clean up unneeded lines

--- setup_things.sh
+++ setup_things.sh
@@ -1,20 +1,5 @@
-# Startup script to launch the node app
-#sudo mv /tmp/app/deployment/myapp.service /lib/systemd/system/myapp.service
-
-# Point Apache to our config
-#sudo mv /tmp/app/deployment/apache_conf/bitnami-apps-prefix.conf /opt/bitnami/apache2/conf/bitnami/bitnami-apps-prefix.conf
-
-
-# Auto-start the service
-#sudo systemctl enable myapp.service
-
 # Move the module into Drupal's location
 sudo mv /tmp/app /opt/bitnami/apps/drupal/htdocs/modules/mymod
 
 # Enable all drupal modules
 cd /opt/bitnami/apps/drupal/htdocs && drush pm-enable robotstxt -y
-
-# With our code in place, restart Apache
-sudo /opt/bitnami/ctlscript.sh restart apache
-
-
