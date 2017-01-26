8383d2372bd03e6f5e07135ab14209d581a72a42
Martin Albisetti <argentina@gmail.com>
2017-01-26 12:22:10 +0100

    Add zone support

--- build_and_deploy-aws.sh
+++ build_and_deploy-aws.sh
@@ -4,17 +4,24 @@ set -ex
 # What zone to build and deploy in
 ZONE=us-east-1
 
-# Log into aws
-#"$GOOGLE_APPLICATION_CREDENTIALS"
+# Build the image and save the output to a log file
+packer build -machine-readable \
+  -var "zone=$ZONE" \
+  -var "custom_image_name=$BUILD_TAG" \ 
+  -var "aws_access_key=AWS_ACCESS_KEY" \
+  -var "aws_secret_key=AWS_SECRET_KEY" \
+  deployment/drupal-aws.packer | tee packer-build-$BUILD_TAG.log
 
-# Build the image
-packer build -var "zone=$ZONE" -var "custom_image_name=$BUILD_TAG" deployment/drupal-aws.packer
+# Extract the ami_id of the built image from the log
+AMI_ID=$(grep 'artifact,0,id' packer-build-$BUILD_TAG.log | cut -d, -f6 | cut -d: -f2)
 
 # The name of the instance to launch, BUILD_TAG is Jenkin's job ID and name
 SERVICE=$BUILD_TAG
 
 # Create and boot the instance
 aws ec2 run-instances \
-  --image-id $SERVICE \
+  --image-id $AMI_ID \
+  --region $ZONE
   --count 1 \
   --instance-type m3.medium
+
