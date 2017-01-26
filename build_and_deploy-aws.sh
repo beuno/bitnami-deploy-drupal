0b5d5a3df561c19b8f3b249ccb672f6f9814f769
Martin Albisetti <argentina@gmail.com>
2017-01-26 12:38:52 +0100

    Bash, bash, bash!

--- build_and_deploy-aws.sh
+++ build_and_deploy-aws.sh
@@ -5,12 +5,7 @@ set -ex
 ZONE=us-east-1
 
 # Build the image and save the output to a log file
-packer build -machine-readable \
-  -var "zone=$ZONE" \
-  -var "custom_image_name=$BUILD_TAG" \ 
-  -var "aws_access_key=$AWS_ACCESS_KEY" \
-  -var "aws_secret_key=$AWS_SECRET_KEY" \
-  deployment/drupal-aws.packer | tee packer-build-$BUILD_TAG.log
+packer build -machine-readable -var "zone=$ZONE" -var "custom_image_name=$BUILD_TAG" -var "aws_access_key=$AWS_ACCESS_KEY" -var "aws_secret_key=$AWS_SECRET_KEY" deployment/drupal-aws.packer | tee packer-build-$BUILD_TAG.log
 
 # Extract the ami_id of the built image from the log
 AMI_ID=$(grep 'artifact,0,id' packer-build-$BUILD_TAG.log | cut -d, -f6 | cut -d: -f2)
