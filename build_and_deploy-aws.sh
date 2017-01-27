fe03e8f387ebba81f4f63781840bab722eef7a55
Martin Albisetti <argentina@gmail.com>
2017-01-27 13:11:07 +0100

    Region all the things

--- build_and_deploy-aws.sh
+++ build_and_deploy-aws.sh
@@ -29,10 +29,17 @@ aws ec2 create-tags --resources $INSTANCE_ID \
   --tags Key=name,Value=$BUILD_TAG
 
 # Wait for the instance to boot
-while state=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --output text --query 'Reservations[*].Instances[*].State.Name'); test "$state" = "pending"; do
+while state=$(aws ec2 describe-instances \
+  --instance-ids $INSTANCE_ID \
+  --output text \
+  --region $ZONE \
+  --query 'Reservations[*].Instances[*].State.Name'); test "$state" = "pending"; do
   sleep 1; echo -n '.'
 done; echo " $state"
 
 # Public IP address of the instance 
-IP_ADDRESS=$(aws ec2 describe-instances --instance-ids $instance_id --output text --query 'Reservations[*].Instances[*].PublicIpAddress')
+IP_ADDRESS=$(aws ec2 describe-instances \
+  --instance-ids $INSTANCE_ID \
+  --output text \
+  --query 'Reservations[*].Instances[*].PublicIpAddress')
 
