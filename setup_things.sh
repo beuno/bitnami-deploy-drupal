78488165a202c359fa860a73e3866c3c9d41e4b3
Martin Albisetti <argentina@gmail.com>
2017-01-30 10:07:44 -0300

    New image, new path

--- setup_things.sh
+++ setup_things.sh
@@ -1,5 +1,9 @@
 # Move the module into Drupal's location
-sudo mv /tmp/app /opt/bitnami/apps/drupal/htdocs/modules/mymod
+# IB path
+#sudo mv /tmp/app /opt/bitnami/apps/drupal/htdocs/modules/mymod
+# nami path
+sudo mv /tmp/app /opt/bitnami/drupal/modules/mymod
 
-# Enable all drupal modules
-cd /opt/bitnami/apps/drupal/htdocs && drush pm-enable robotstxt -y
+# Enable our drupal module
+#XXX Need to dynamically specify the module name
+cd /opt/bitnami/drupal && drush pm-enable robotstxt -y
