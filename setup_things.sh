00ba341ca5209b4f2681c8cabd6cd46d922994fa
Martin Albisetti <argentina@gmail.com>
2017-02-03 10:06:07 -0300

    Fix typo

--- setup_things.sh
+++ setup_things.sh
@@ -3,12 +3,11 @@
 #sudo mv /tmp/app /opt/bitnami/apps/drupal/htdocs/modules/mymod
 
 # Move Drupal out of the way
-sudo nami stop apache; nami stop php
 sudo mv /opt/bitnami/drupal /opt/bitnami/drupal.bitnami.orig
 
 # nami path
 sudo mv /tmp/app /opt/bitnami/drupal
-cd /opt/bitnami/drupal && composer install %% drush cr
+cd /opt/bitnami/drupal && composer install && drush cr
 
 
 # Enable our drupal module
