cb8ae410f26b1af7822b88ee9d4b4371a97690d2
Martin Albisetti <argentina@gmail.com>
2017-02-03 10:55:17 -0300

    Set permissions

--- setup_things.sh
+++ setup_things.sh
@@ -7,8 +7,13 @@ sudo mv /opt/bitnami/drupal /opt/bitnami/drupal.bitnami.orig
 
 # nami path
 sudo mv /tmp/app /opt/bitnami/drupal
+
+# Download all project dependencies
 cd /opt/bitnami/drupal && composer install
 
+# Set permissions for Apache
+#XXX needs review
+sudo chown daemon:daemon /opt/bitnami/drupal/sites/* -R
 
 # Enable our drupal module
 #XXX Need to dynamically specify the module name
